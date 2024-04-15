0f0fb6950ff71290f5a95ad65793ed3aa1e6012b
studern-bzz <studern@bzz.ch>
2024-04-15 10:50:17 +0200

    Initial commit

---
+++ Tag1/Aufg.1.2/baby-steps.js
@@ -0,0 +1,7 @@
+let numbers  = process.argv
+let sum = 0
+for (let i = 2; i < numbers.length; i++) {
+    sum += Number(numbers[i]);
+}
+console.log(sum)
+
---
+++ Tag1/Aufg.1.2/filtered-ls.js
@@ -0,0 +1,3 @@
+const fs = require('fs')
+let dir = process.argv[2]
+let extension = process.argv[3]
